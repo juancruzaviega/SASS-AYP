562bb5ce969af616ed67bec82255f75e91df1b51
Juan Aviega <juancruzaviega@gmail.com>
2022-08-11 11:19:38 -0300

    agrego fetch

--- js/appAviega.js
+++ js/appAviega.js
@@ -1,88 +1,200 @@
-let productos = [
-{ id: 100, nombre: "Entrada Campo", precio: 1550 },
-{ id: 101, nombre: "Entrada Platea", precio: 2500 },
-{ id: 102, nombre: "Entrada VIP + Meet & Greet", precio: 3500 },
-];
+// let productos = [
+// { id: 100, nombre: "Entrada Campo", precio: 1550 },
+// { id: 101, nombre: "Entrada Platea", precio: 2500 },
+// { id: 102, nombre: "Entrada VIP + Meet & Greet", precio: 3500 },
+// ];
 
-let aux = localStorage.getItem("productosEnCarro");
+// let aux = localStorage.getItem("productosEnCarro");
+// let productosEnCarro;
+
+// if (!aux) {
+// productosEnCarro = [];
+// } else {
+// productosEnCarro = JSON.parse(aux);
+// pintarCarrito();
+// }
+
+// function pintarListado() {
+// let aux = "";
+// for (let i = 0; i < productos.length; i++) {
+//     aux =
+// aux +
+// `<div onclick="meterAlCarro({id: ${productos[i].id}, nombre: '${productos[i].nombre}', precio: ${productos[i].precio}})" style="cursor: pointer; border: 1px solid grey;">
+// <h3> Nombre: ${productos[i].nombre} </h3>
+// <p> Precio: $ ${productos[i].precio} </p>
+// <p> ID: ${productos[i].id} </p>
+//     </div>`;
+// }
+// document.getElementById("div-productos").innerHTML = aux;
+// }
+// pintarListado();
+
+// function meterAlCarro(objetosProducto) {
+// productosEnCarro.push(objetosProducto);
+// localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
+// pintarCarrito();
+// }
+
+// function borrarDelCarro(id) {
+// productosEnCarro.splice(id, 1);
+// localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
+// pintarCarrito();
+// }
+
+// function pintarCarrito() {
+// let aux = "";
+// for (let i = 0; i < productosEnCarro.length; i++) {
+//     aux =
+// aux +
+// `<div>
+// <h3> Nombre: ${productosEnCarro[i].nombre} </h3>
+// <p> Precio: $ ${productosEnCarro[i].precio} </p>
+// <p> ID: ${productosEnCarro[i].id} </p>
+// <p onclick="borrarDelCarro(${i})" style="cursor: pointer; border: 2px solid red; display: inline-block;"> Borrar del carro 🗑️ </p>
+//     </div>`;
+// }
+// document.getElementById("div-carrito").innerHTML = aux;
+// }
+
+// function manejeElClick() {
+//     Toastify({
+//         text: "Producto agregado al carro!",
+//         duration: 3000,
+//         gravity: 'bottom',
+//         position: 'left',
+        
+//         style: {
+//             background: 'linear-gradient(to right, black, green)',
+            
+//         }
+//     }).showToast();
+
+// }
+
+// function manejeElClickBorrar() {
+//     Toastify({
+//         text: "Producto borrado del carro!",
+//         duration: 3000,
+//         gravity: 'bottom',
+//         position: 'left',
+        
+//         style: {
+//             background: 'linear-gradient(to right, black, red)',
+            
+//         }
+//     }).showToast();
+
+// }
+
+fetch('./data.json')
+    .then((resinicial) => resinicial.json())
+    .then((res) => {
+        const entradas = res;
+
+        let htmlAux = '';
+        for (let i = 0; i < entradas.length; i++) {
+            htmlAux = htmlAux +
+            `<div onclick="meterAlCarro(${entradas[i].id});">
+                <h3>${entradas[i].name}</h3>
+                <p>${entradas[i].precio}</p>
+
+            </div>`;
+        }
+        document.getElementById('listadoDeProductos').innerHTML = htmlAux;
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+let aux = localStorage.getItem('productosEnCarro');
 let productosEnCarro;
 
 if (!aux) {
-productosEnCarro = [];
-} else {
-productosEnCarro = JSON.parse(aux);
-pintarCarrito();
-}
+    productosEnCarro = [];
+    } else {
+    productosEnCarro = JSON.parse(aux);
+    pintarCarrito();
+    }
+
+
+
 
 function pintarListado() {
-let aux = "";
-for (let i = 0; i < productos.length; i++) {
-    aux =
-aux +
-`<div onclick="meterAlCarro({id: ${productos[i].id}, nombre: '${productos[i].nombre}', precio: ${productos[i].precio}})" style="cursor: pointer; border: 1px solid grey;">
-<h3> Nombre: ${productos[i].nombre} </h3>
-<p> Precio: $ ${productos[i].precio} </p>
-<p> ID: ${productos[i].id} </p>
-    </div>`;
-}
-document.getElementById("div-productos").innerHTML = aux;
-}
-pintarListado();
-
-function meterAlCarro(objetosProducto) {
-productosEnCarro.push(objetosProducto);
-localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
-pintarCarrito();
-}
-
-function borrarDelCarro(id) {
-productosEnCarro.splice(id, 1);
-localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
-pintarCarrito();
-}
-
-function pintarCarrito() {
-let aux = "";
-for (let i = 0; i < productosEnCarro.length; i++) {
-    aux =
-aux +
-`<div>
-<h3> Nombre: ${productosEnCarro[i].nombre} </h3>
-<p> Precio: $ ${productosEnCarro[i].precio} </p>
-<p> ID: ${productosEnCarro[i].id} </p>
-<p onclick="borrarDelCarro(${i})" style="cursor: pointer; border: 2px solid red; display: inline-block;"> Borrar del carro 🗑️ </p>
-    </div>`;
-}
-document.getElementById("div-carrito").innerHTML = aux;
-}
-
-function manejeElClick() {
-    Toastify({
-        text: "Producto agregado al carro!",
-        duration: 3000,
-        gravity: 'bottom',
-        position: 'left',
-        
-        style: {
-            background: 'linear-gradient(to right, black, green)',
-            
+        let entradas = "";
+        for (let i = 0; i < entradas.length; i++) {
+            aux =
+        aux +
+        `<div onclick="meterAlCarro({id: ${entradas[i].id}, nombre: '${entradas[i].name}', precio: ${entradas[i].precio}})" style="cursor: pointer; border: 1px solid grey;">
+        <h3> Nombre: ${entradas[i].name} </h3>
+        <p> Precio: $ ${entradas[i].precio} </p>
+        <p> ID: ${entradas[i].id} </p>
+            </div>`;
         }
-    }).showToast();
-
-}
-
-function manejeElClickBorrar() {
-    Toastify({
-        text: "Producto borrado del carro!",
-        duration: 3000,
-        gravity: 'bottom',
-        position: 'left',
-        
-        style: {
-            background: 'linear-gradient(to right, black, red)',
-            
+        document.getElementById("listadoDeProductos").innerHTML = aux;
         }
-    }).showToast();
+        pintarListado();
 
-}
 
+        function meterAlCarro(id) {
+            productosEnCarro.push(id);
+            localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
+            pintarCarrito();
+            }
+
+
+            function borrarDelCarro(id) {
+                productosEnCarro.splice(id, 1);
+                localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
+                pintarCarrito();
+                }
+
+
+
+
+
+
+            function pintarCarrito() {
+                let aux = "";
+                for (let i = 0; i < productosEnCarro.length; i++) {
+                    aux =
+                aux +
+                `<div>
+                    <h3> Nombre: ${productosEnCarro[i].name} </h3>
+                    <p> Precio: $ ${productosEnCarro[i].precio} </p>
+                    <p> ID: ${productosEnCarro[i].id} </p>
+                    <p onclick="borrarDelCarro(${i})" style="cursor: pointer; border: 2px solid red; display:       inline-block;"> Borrar del carro 🗑️ </p>
+                </div>`;
+                }
+                document.getElementById("div-carrito").innerHTML = aux;
+                }
+
+
+                function manejeElClick() {
+                    Toastify({
+                        text: "Producto agregado al carro!",
+                        duration: 3000,
+                        gravity: 'bottom',
+                        position: 'left',
+                        
+                        style: {
+                            background: 'linear-gradient(to right, black, green)',
+                            
+                        }
+                    }).showToast();
+                
+                }
+                
+                function manejeElClickBorrar() {
+                    Toastify({
+                        text: "Producto borrado del carro!",
+                        duration: 3000,
+                        gravity: 'bottom',
+                        position: 'left',
+                        
+                        style: {
+                            background: 'linear-gradient(to right, black, red)',
+                            
+                        }
+                    }).showToast();
+                
+                }
